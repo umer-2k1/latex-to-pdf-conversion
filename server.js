0e09c2f063d3c55d39454078da2488cd5e2bef18
umer-2k1 <117743102+umer-2k1@users.noreply.github.com>
2025-12-02 11:37:47 +0500

    feat: add latex to pdf conversion api

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const { exec } = require("child_process");
+const crypto = require("crypto");
+
+const app = express();
+app.use(express.text({ limit: "5mb" }));
+
+const PORT = 3000;
+
+app.get("/health-check", async (req, res) => {
+  try {
+    return res.status(200).json({ data: "API success" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error in health check API.");
+  }
+});
+
+app.post("/compile-latex", async (req, res) => {
+  const resume = req.body;
+
+  if (!resume) {
+    return res
+      .status(400)
+      .send("Request body must include 'resume' field with LaTeX content.");
+  }
+
+  const outputDir = path.join(__dirname, "output");
+  if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
+  const id = crypto.randomBytes(6).toString("hex");
+  const texFile = path.join(outputDir, `${id}.tex`);
+  const pdfFile = path.join(outputDir, `${id}.pdf`);
+  // const texFile = path.join(__dirname, "resume_01.tex"); // <-- your LaTeX file
+
+  try {
+    fs.writeFileSync(texFile, resume);
+    const pdflatexPath = `"C:\\Users\\lenovo\\AppData\\Local\\Programs\\MiKTeX\\miktex\\bin\\x64\\pdflatex.exe"`;
+
+    // Compile LaTeX to PDF
+    await new Promise((resolve, reject) => {
+      exec(
+        `${pdflatexPath} -interaction=nonstopmode -output-directory="${outputDir}" "${texFile}"`,
+        (error, stdout, stderr) => {
+          console.log("pdflatex stdout:\n", stdout);
+          console.log("pdflatex stderr:\n", stderr);
+          if (error) return reject(stderr || stdout || error);
+          resolve(stdout);
+        }
+      );
+    });
+    if (!fs.existsSync(pdfFile)) {
+      return res.status(500).send("PDF was not generated.");
+    }
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", `inline; filename=${id}.pdf`);
+    res.send(fs.readFileSync(pdfFile));
+    console.log(`PDF compiled successfully and saved to212: ${pdfFile}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Failed to compile LaTeX. Check pdflatex output.");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`LaTeX API server running at http://localhost:${PORT}`);
+});
